4e8480990345371f38de3d984f5e3718aab73f4d
ChoFongCit <cho.leong@mycit.ie>
2024-10-20 14:29:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-console.log("hello guys")
+
 const express = require("express")
 const app = express()
 
+app.get("/",(req,res)=>{
+    console.log("hi gaes")
+    res.send("Hi")
+})
+
 const port = 8000
+
+app.listen(port)
