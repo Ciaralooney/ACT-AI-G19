1ff30697a52b4f8f3735445e2056ffbcf0e42edd
Ciaralooney <66528901+Ciaralooney@users.noreply.github.com>
2024-10-20 15:54:06 +0100

    Installed backend packages needed. I also downloaded nodemon as it is best so when you save the files you don’t have to manually refresh the server, it is done for you

---
+++ server.js
@@ -0,0 +1,23 @@
+
+const express = require("express")
+const mongoose = require('mongoose')
+const app = express()
+
+app.get("/",(req,res)=>{
+    console.log("hi gaes")
+    res.send("Hi")
+})
+
+const port = 8000
+app.listen(port)
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/loginRoute', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
