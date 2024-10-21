6a9cf4f9c56d35cf5fb86830a8bdfc3105ed7f87
Ciaralooney <66528901+Ciaralooney@users.noreply.github.com>
2024-10-21 10:34:14 +0100

    fixed home route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const homeRouter = require('./routes/homeRouter');
 
 const app = express();
 const url = "mongodb://127.0.0.1:27017/loginRoute"
+const port = 3000;
 
 // view engine setup
 app.set('view engine', 'ejs'); // specifying the view engine in the express app
@@ -32,6 +33,12 @@ mongoose.connect(url)
   .catch((error) => {
     console.error('Error connecting to server:', error);
   });
-
+  
+  
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+  
+  
   // exporting the app object to make it available in other files 
   module.exports = app;
