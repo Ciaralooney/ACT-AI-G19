35e72132e0af7d594c682a64c11a9dc886663302
Veronikacodes <35336415+veragrand@users.noreply.github.com>
2024-11-17 18:56:47 +0000

    basic rating feature

--- routes/ratingRouter.js
+++ routes/ratingRouter.js
@@ -7,7 +7,7 @@ const ratingRouter = express.Router(); // Creating the router instance
 
 // Display the rating form (GET /rating -> mapped from server.js)
 ratingRouter.get('/', (req, res) => {
-    res.render('rating_copy'); // Render the rating form from views/rating.ejs
+    res.render('rating'); // Render the rating form from views/rating.ejs
 });
 
 // Handle rating submission (POST /rating/submit-rating -> mapped from server.js)
