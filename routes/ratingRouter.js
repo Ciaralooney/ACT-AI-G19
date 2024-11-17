8c4cd831c6e3d32871853cc3ad640a82842f4836
Oleksandr “Sasha” Mitichkin <infoformiti@gmail.com>
2024-11-17 22:07:59 +0000

    added style.css
    Added link to the header

--- routes/ratingRouter.js
+++ routes/ratingRouter.js
@@ -7,7 +7,7 @@ const ratingRouter = express.Router(); // Creating the router instance
 
 // Display the rating form (GET /rating -> mapped from server.js)
 ratingRouter.get('/', (req, res) => {
-    res.render('rating'); // Render the rating form from views/rating.ejs
+    res.render('rating', {title: "Rate us!"}); // Render the rating form from views/rating.ejs
 });
 
 // Handle rating submission (POST /rating/submit-rating -> mapped from server.js)
@@ -19,7 +19,7 @@ ratingRouter.post('/submit-rating', async (req, res) => {
         const newRating = new Rating({ rating, comment });
         await newRating.save();
 
-        res.send('Thank you for your feedback!'); // Respond with confirmation
+        res.send('Thank you for your feedback!'); // Respond with confirmation        
     } catch (error) {
         console.error('Error saving rating:', error);
         res.status(500).send('An error occurred. Please try again later.');
