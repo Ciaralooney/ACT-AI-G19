8f6324c75cfb5291ab25da167e9579d4f07ce64c
Ciaralooney <66528901+Ciaralooney@users.noreply.github.com>
2024-12-22 18:18:00 +0000

    Update users.js

--- models/users.js
+++ models/users.js
@@ -4,10 +4,10 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    role: { type: String, default: "user" },
     resetToken: { type: String, required: false }, 
     resetTokenExpiration: { type: Date, required: false } 
 });
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
-
